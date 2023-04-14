9750455292de7ad00c6fb818e97059c5ec46919f
paxnamra <bialyfeniks4@gmail.com>
2023-04-15 01:55:08 +0200

    added path variable converter to title case, added nonNull annotation in controller params

--- src/main/java/cities/controllers/CityController.java
+++ src/main/java/cities/controllers/CityController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,7 @@ public class CityController {
     }
 
     @GetMapping("/{cityName}")
-    public ResponseEntity<City> getCityByName(@PathVariable String cityName) {
+    public ResponseEntity<City> getCityByName(@PathVariable @NonNull String cityName) {
         Optional<City> optionalCity = cityService.getCityByName(cityName);
         return optionalCity.map(city -> ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(city))
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.APPLICATION_JSON).build());
--- src/main/java/cities/services/CityService.java
+++ src/main/java/cities/services/CityService.java
@@ -5,8 +5,10 @@ import cities.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CityService {
@@ -19,6 +21,12 @@ public class CityService {
     }
 
     public Optional<City> getCityByName(String name) {
-        return repository.findCityByName(name);
+        return repository.findCityByName(titleCasePath(name));
+    }
+
+    private String titleCasePath(String pathVariable) {
+        return Arrays.stream(pathVariable.split("\\s+"))
+                .map(word -> word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase())
+                .collect(Collectors.joining(" "));
     }
 }
